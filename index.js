2750cef7a73d5787e64070b9a48712d8a4b031c8
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-11-27 17:51:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ function dec(n) {
 
 function makeInt(n) {
   if () {
-    
+
   }
   else if () {
-    
+
   }
   else {
     return NaN
-  }  
+  }
 }
 
 function preserveDecimal(n) {
   if () {
-    
+
   }
   else {
     return NaN
